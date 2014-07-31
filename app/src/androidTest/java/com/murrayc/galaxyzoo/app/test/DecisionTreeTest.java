bce67914a6c5e7c81ca858a0804dafb48c23a707
Murray Cumming <murrayc@murrayc.com>
2014-07-31 09:13:06 +0200

    Add a DecisionTree XML parser and a test for it.

---
+++ app/src/androidTest/java/com/murrayc/galaxyzoo/app/test/DecisionTreeTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2009, 2010, 2011 Openismus GmbH
+ *
+ * This file is part of android-glom
+ *
+ * android-glom is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * android-glom is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with android-glom.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.murrayc.galaxyzoo.app.test;
+
+import android.test.AndroidTestCase;
+
+import com.murrayc.galaxyzoo.app.DecisionTree;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Simple test to ensure that the generated bindings are working.
+ */
+public class DecisionTreeTest extends AndroidTestCase {
+    private static InputStream inputStream;
+    private static DecisionTree decisionTree;
+
+
+    @Override
+    public void setUp() {
+        //For some reason DecisionTreeTest.class.getResourceAsStream() doesn't work,
+        //so we use DecisionTreeTest.class.getClassLoader().getResourceAsStream(), which does.
+        inputStream = DecisionTreeTest.class.getClassLoader().getResourceAsStream("test_decision_tree.xml");
+        assertNotNull(inputStream);
+
+        decisionTree = new DecisionTree(inputStream);
+    }
+
+    @Override
+    public void tearDown() {
+    }
+
+    public void testSize() {
+        assertNotNull(decisionTree);
+        assertNotNull(decisionTree.questionsMap);
+        assertEquals(12, decisionTree.questionsMap.size());
+    }
+
+    public void testQuestions() {
+        final String QUESTION_ID = "sloan-3";
+        final DecisionTree.Question question = decisionTree.getQuestion(QUESTION_ID);
+        assertEquals(QUESTION_ID, question.getId());
+        assertEquals("Is there any sign of a spiral arm pattern?", question.getText());
+
+        final DecisionTree.Question nextQuestion = decisionTree.getNextQuestionForAnswer(QUESTION_ID, "a-1");
+        assertEquals("sloan-4", nextQuestion.getId());
+
+        //TODO: Test getQuestion() and getNextQuestion().
+    }
+
+}
---
+++ app/src/main/java/com/murrayc/galaxyzoo/app/DecisionTree.java
@@ -0,0 +1,253 @@
+/*
+ * Copyright (C) 2014 Murray Cumming
+ *
+ * This file is part of android-galaxyzoo.
+ *
+ * android-galaxyzoo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * android-galaxyzoo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with android-galaxyzoo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.murrayc.galaxyzoo.app;
+
+import android.text.TextUtils;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * Created by murrayc on 7/30/14.
+ */
+public class DecisionTree {
+
+    private static final String NODE_ROOT = "murrayc_zoonverse_questions";
+    private static final String NODE_QUESTION = "question";
+    private static final String NODE_ANSWER = "answer";
+
+    static class Answer {
+        public String id;
+        public String text;
+        public String icon;
+        public String leadsToQuestionId;
+
+        Answer(final String id, final String text, final String icon, final String leadsToQuestionId) {
+            this.id = id;
+            this.text = text;
+            this.icon = icon;
+            this.leadsToQuestionId = leadsToQuestionId;
+        }
+
+        public String getId() {
+            return id;
+        }
+    }
+
+    public static class Question {
+        public String id;
+        public String text;
+        public String help;
+
+        public final Hashtable<String, Answer> answersMap = new Hashtable<>();
+
+        Question(final String id, final String text, final String help) {
+            this.id = id;
+            this.text = text;
+            this.help = help;
+        }
+
+
+        public String getId() {
+            return id;
+        }
+
+        public String getText() {
+            return text;
+        }
+    }
+
+    //TODO: Make this private and add accessors.
+    public final Hashtable<String, Question> questionsMap = new Hashtable<>();
+
+    public DecisionTree(final InputStream inputStream) {
+        final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder;
+        try {
+            documentBuilder = dbf.newDocumentBuilder();
+        } catch (final ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return;
+        }
+
+        org.w3c.dom.Document xmlDocument;
+
+        try {
+            xmlDocument = documentBuilder.parse(inputStream);
+        } catch (final SAXException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return;
+        } catch (final IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return;
+        }
+
+        final Element rootNode = xmlDocument.getDocumentElement();
+        if (!TextUtils.equals(rootNode.getNodeName(), NODE_ROOT)) {
+            android.util.Log.v("android-galaxyzoo", "Unexpected XML root node name found: " + rootNode.getNodeName());
+            return;
+        }
+
+        final List<Node> listQuestions = getChildrenByTagName(rootNode, NODE_QUESTION);
+        for (final Node node : listQuestions) {
+            if (!(node instanceof Element)) {
+                continue;
+            }
+
+            final Element element = (Element) node;
+            final Question question = loadQuestion(element);
+            questionsMap.put(question.getId(), question);
+        }
+    }
+
+    public Question getQuestion(final String questionId) {
+        if (questionsMap == null) {
+            return null;
+        }
+
+        return questionsMap.get(questionId);
+    }
+
+
+    public Question getNextQuestionForAnswer(final String questionId, final String answerId) {
+        final Question question = getQuestion(questionId);
+        if (question == null) {
+            return null;
+        }
+
+        if (question.answersMap == null) {
+            return null;
+        }
+
+        final Answer answer = question.answersMap.get(answerId);
+        if (answer == null) {
+            return null;
+        }
+
+        return getQuestion(answer.leadsToQuestionId);
+    }
+
+    /**
+     * getElementsByTagName() is recursive, but we do not want that.
+     *
+     * @param parentNode
+     * @param tagName
+     * @return
+     */
+    private List<Node> getChildrenByTagName(final Element parentNode, final String tagName) {
+        final List<Node> result = new ArrayList<>();
+
+        final NodeList list = parentNode.getElementsByTagName(tagName);
+        final int num = list.getLength();
+        for (int i = 0; i < num; i++) {
+            final Node node = list.item(i);
+            if (node == null) {
+                continue;
+            }
+
+            final Node itemParentNode = node.getParentNode();
+            if (itemParentNode.equals(parentNode)) {
+                result.add(node);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * getElementsByTagName() is recursive, but we do not want that.
+     *
+     * @param parentNode
+     * @param tagName
+     * @return
+     */
+    private Node getFirstChildByTagName(final Element parentNode, final String tagName) {
+        final List<Node> result = new ArrayList<>();
+
+        final NodeList list = parentNode.getElementsByTagName(tagName);
+        final int num = list.getLength();
+        for (int i = 0; i < num; i++) {
+            final Node node = list.item(i);
+            if (node == null) {
+                continue;
+            }
+
+            final Node itemParentNode = node.getParentNode();
+            if (itemParentNode.equals(parentNode)) {
+                return node;
+            }
+        }
+
+        return null;
+    }
+
+    private String getTextOfChildNode(final Element element, final String tagName) {
+        final Node node = getFirstChildByTagName(element, tagName);
+        if (node == null)
+            return null;
+
+        return node.getTextContent();
+    }
+
+    private Question loadQuestion(final Element questionNode) {
+        final Question result = new Question(
+                questionNode.getAttribute("id"),
+                getTextOfChildNode(questionNode, "text"),
+                getTextOfChildNode(questionNode, "help"));
+
+
+        final List<Node> listAnswers = getChildrenByTagName(questionNode, NODE_ANSWER);
+        for (final Node node : listAnswers) {
+            if (!(node instanceof Element)) {
+                continue;
+            }
+
+            final Element element = (Element) node;
+            final Answer answer = loadAnswer(element);
+            result.answersMap.put(answer.getId(), answer);
+        }
+
+        return result;
+    }
+
+    private Answer loadAnswer(final Element answerNode) {
+        final Answer result = new Answer(
+                answerNode.getAttribute("id"),
+                getTextOfChildNode(answerNode, "text"),
+                answerNode.getAttribute("icon"),
+                answerNode.getAttribute("leadsTo"));
+        return result;
+    }
+}
