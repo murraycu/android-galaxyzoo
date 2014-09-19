fda13b951db99765351456439dfda9dca3d9b537
Murray Cumming <murrayc@murrayc.com>
2014-09-19 13:59:09 +0200

    Add a simple test for SubjectViewerActivity.

---
+++ app/src/androidTest/java/com/murrayc/galaxyzoo/app/test/SubjectViewerActivityTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2014 Murray Cumming
+ *
+ * This file is part of android-galaxyzoo.
+ *
+ * android-galaxyzoo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * android-galaxyzoo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with android-galaxyzoo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.murrayc.galaxyzoo.app.test;
+
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+
+import com.murrayc.galaxyzoo.app.SubjectViewerActivity;
+
+/**
+ * Created by murrayc on 5/26/14.
+ */
+public class SubjectViewerActivityTest
+        extends ActivityUnitTestCase<SubjectViewerActivity> {
+
+    private SubjectViewerActivity mActivity;
+
+    public SubjectViewerActivityTest() {
+        super(SubjectViewerActivity.class);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        startActivity(new Intent(getInstrumentation().getTargetContext(), SubjectViewerActivity.class), null, null);
+
+        mActivity = getActivity();
+        assertNotNull(mActivity);
+    }
+
+    public void testExists() {
+        assertNotNull(mActivity);
+    }
+}
