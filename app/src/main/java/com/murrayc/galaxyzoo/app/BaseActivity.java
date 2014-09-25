c1d70fec184f2ce093d909f08f5d3807beb257ed
Murray Cumming <murrayc@murrayc.com>
2014-09-25 19:06:20 +0200

    BaseActivity.onOptionsItemSelected(): Handle up more generically.
    This seems to be the recommended way (which also support earlier API targets)
    and which should actually use the parent that is specified in the manifest.

--- app/src/main/java/com/murrayc/galaxyzoo/app/BaseActivity.java
+++ app/src/main/java/com/murrayc/galaxyzoo/app/BaseActivity.java
@@ -24,6 +24,7 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
 import android.text.TextUtils;
 import android.view.MenuItem;
 
@@ -64,16 +65,7 @@ class BaseActivity extends FragmentActivity {
         final int id = item.getItemId();
 
         if (id == android.R.id.home) {
-            //Derived Activities should handle this.
-
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. Use NavUtils to allow users
-            // to navigate up one level in the application structure. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
-            navigateUpTo(new Intent(this, ListActivity.class));
+            NavUtils.navigateUpFromSameTask(this);
             return true;
         }
 
