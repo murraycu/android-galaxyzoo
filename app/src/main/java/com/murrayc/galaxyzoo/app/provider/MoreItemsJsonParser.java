7214996e19bd34a848e0e05a7d7e145f94807252
Murray Cumming <murrayc@murrayc.com>
2014-10-08 11:21:23 +0200

    MoreItemsJsonParser: Use JsonReader instead of JsonTokener.
    So we can parse the InputStream directly instead of copying its contents
    out to a String first.

--- app/src/main/java/com/murrayc/galaxyzoo/app/provider/MoreItemsJsonParser.java
+++ app/src/main/java/com/murrayc/galaxyzoo/app/provider/MoreItemsJsonParser.java
@@ -1,14 +1,13 @@
 package com.murrayc.galaxyzoo.app.provider;
 
-import com.murrayc.galaxyzoo.app.Log;
+import android.util.JsonReader;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
+import com.murrayc.galaxyzoo.app.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,70 +16,88 @@ import java.util.List;
  */
 public class MoreItemsJsonParser {
     public static List<Subject> parseMoreItemsResponseContent(final InputStream content) {
-        final String str;
-        try {
-            str = HttpUtils.getStringFromInputStream(content);
-        } catch (IOException e) {
-            Log.error("parseMoreItemsResponseContent(): Exception while getting string from input stream", e);
-            return null;
-        }
-
         final List<Subject> result = new ArrayList<>();
 
-        final JSONTokener tokener = new JSONTokener(str);
-        JSONArray jsonArray;
+        final JsonReader reader;
         try {
-            jsonArray = new JSONArray(tokener);
-        } catch (JSONException e) {
-            Log.error("JSON parsing failed.", e);
-            return result;
+            reader = new JsonReader(new InputStreamReader(content, "UTF-8"));
+            reader.beginArray();
+            while (reader.hasNext()) {
+                while (reader.hasNext()) {
+                    final Subject subject = parseMoreItemsJsonObjectSubject(reader);
+                    if (subject != null) {
+                        result.add(subject);
+                    }
+                }
+            }
+            reader.endArray();
+            reader.close();
+        } catch (final UnsupportedEncodingException e) {
+            Log.info("parseMoreItemsResponseContent: UnsupportedEncodingException parsing JSON", e);
+        } catch (final IOException e) {
+            Log.info("parseMoreItemsResponseContent: IOException parsing JSON", e);
+        } catch (final IllegalStateException e) {
+            Log.info("parseMoreItemsResponseContent: IllegalStateException parsing JSON", e);
         }
 
-        for (int i = 0; i < jsonArray.length(); ++i) {
-            JSONObject obj;
-            try {
-                obj = jsonArray.getJSONObject(i);
-            } catch (JSONException e) {
-                Log.error("JSON parsing of object failed.", e);
-                return result;
-            }
-
-            final Subject subject = parseMoreItemsJsonObjectSubject(obj);
-            if (subject != null) {
-                result.add(subject);
-            }
-        }
-
-        //TODO: If this is 0 then something went wrong. Let the user know,
-        // only flush old state now that new state has arrived
         if (result.size() == 0) {
             Log.error("Failed. No JSON entities parsed."); //TODO: Use some constant error code?
         }
 
+        //TODO: If this is 0 then something went wrong. Let the user know,
         //maybe via the parseMoreItemsJsonObjectSubject() return string..
         //For instance, the Galaxy-Zoo server could be down for maintenance (this has happened before),
         //or there could be some other network problem.
         return result;
     }
 
-    private static Subject parseMoreItemsJsonObjectSubject(final JSONObject objSubject) {
-        try {
-            final Subject subject = new Subject();
-            subject.mId = objSubject.getString("id");
-            subject.mZooniverseId = objSubject.getString("zooniverse_id");
-            final JSONObject objLocation = objSubject.getJSONObject("location");
-            if (objLocation != null) {
-                subject.mLocationStandard = objLocation.getString("standard");
-                subject.mLocationThumbnail = objLocation.getString("thumbnail");
-                subject.mLocationInverted = objLocation.getString("inverted");
-            }
+    private static Subject parseMoreItemsJsonObjectSubject(final JsonReader reader) throws IOException {
+        reader.beginObject();
 
-            return subject;
-        } catch (JSONException e) {
-            Log.error("JSON parsing of object fields failed.", e);
+        final Subject result = new Subject();
+
+        while (reader.hasNext()) {
+            final String name = reader.nextName();
+            switch (name) {
+                case "id":
+                    result.mId = reader.nextString();
+                    break;
+                case "zooniverse_id":
+                    result.mZooniverseId = reader.nextString();
+                    break;
+                case "location":
+                    parseMoreItemsJsonObjectSubjectLocation(reader, result);
+                    break;
+                default:
+                    reader.skipValue();
+            }
         }
 
-        return null;
+        reader.endObject();
+        return result;
+    }
+
+    private static void parseMoreItemsJsonObjectSubjectLocation(final JsonReader reader, final Subject result) throws IOException {
+        reader.beginObject();
+
+        while (reader.hasNext()) {
+            final String name = reader.nextName();
+            switch (name) {
+                case "standard":
+                    result.mLocationStandard = reader.nextString();
+                    break;
+                case "thumbnail":
+                    result.mLocationThumbnail= reader.nextString();
+                    break;
+                case "inverted":
+                    result.mLocationInverted = reader.nextString();
+                    break;
+                default:
+                    reader.skipValue();
+            }
+        }
+
+        reader.endObject();
     }
 
     public static class Subject {
