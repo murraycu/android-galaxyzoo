e0bb820a7e6f5081f605eb428c4a9348d80dab0c
Murray Cumming <murrayc@murrayc.com>
2014-10-08 09:19:44 +0200

    ItemsContentProvider: Avoid crashes during test cases due to MockContext limitations.

--- app/src/main/java/com/murrayc/galaxyzoo/app/provider/ItemsContentProvider.java
+++ app/src/main/java/com/murrayc/galaxyzoo/app/provider/ItemsContentProvider.java
@@ -603,6 +603,9 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
                 // Even if no URI is provided by the client, we still create the local URI and put
                 // it in the table row for later use.
                 final ContentValues valuesComplete = new ContentValues(values);
+
+                //This doesn't actually get any data from the locations - we provide them
+                //just to have them in the listFiles items together with the created content uris.
                 final List<CreatedFileUri> listFiles = createFileUrisForImages(valuesComplete,
                         subjectId,
                         values.getAsString(Item.Columns.LOCATION_STANDARD_URI),
@@ -762,6 +765,14 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
      * @param asyncFileDownloads Get the image data asynchronously if this is true.
      */
     private boolean cacheUriToFile(final String uriFileToCache, final String cacheFileUri, final String subjectId, final ImageType imageType, boolean asyncFileDownloads) {
+        if (TextUtils.isEmpty(uriFileToCache)) {
+            return false;
+        }
+
+        if (TextUtils.isEmpty(cacheFileUri)) {
+            return false;
+        }
+
         throwIfNoNetwork();
 
         if (asyncFileDownloads) {
@@ -828,7 +839,12 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
         //Download enough subjects:
         queueRegularTasks();
 
-        PreferenceManager.getDefaultSharedPreferences(getContext()).registerOnSharedPreferenceChangeListener(this);
+        try {
+            PreferenceManager.getDefaultSharedPreferences(getContext()).registerOnSharedPreferenceChangeListener(this);
+        } catch (final UnsupportedOperationException e) {
+            //This happens during our test case, because the MockContext doesn't support this,
+            //so ignore this.
+        }
 
         return true;
     }
