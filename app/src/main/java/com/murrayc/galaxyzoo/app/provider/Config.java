62b511f92e8aa0d7aa3a0b72f3d91b96ba37327d
Murray Cumming <murrayc@murrayc.com>
2014-10-09 09:04:08 +0200

    Put back some whitespace that I like.

--- app/src/main/java/com/murrayc/galaxyzoo/app/provider/Config.java
+++ app/src/main/java/com/murrayc/galaxyzoo/app/provider/Config.java
@@ -8,15 +8,18 @@ public class Config {
     //TODO: This is used in the app, not in the content provider.
     //Maybe this whole class should be in the app, not the content provider.
     public static final String TALK_URI = "http://talk.galaxyzoo.org/#/subjects/";
+
     //TODO: This is used in the app, not in the content provider.
     //Maybe this whole class should be in the app, not the content provider.
     public static final String EXAMINE_URI = "http://www.galaxyzoo.org/#/examine/";
+
     //TODO: This is used in the app, not in the content provider.
     //Maybe this whole class should be in the app, not the content provider.
     //TODO: Is there a stable URI for this?
     public static final String ICONS_CSS_URI = "http://www.galaxyzoo.org/application-2014-09-05_13-06-46.css";
 
     static private final String SERVER = "https://api.zooniverse.org/projects/galaxy_zoo/";
+
     /**
      * REST uri for querying items.
      * Like, the Galaxy-Zoo website's code, this hard-codes the Group ID for the Sloan survey:
@@ -28,6 +31,7 @@ public class Config {
     static final String LOGIN_URI =
             //"http://www.murrayc.com/galaxyzootestlogin"; //Avoid bothering the zooniverse server until we are more sure that this works.
             SERVER + "login";
+
     static private final String STATIC_IMAGES_SERVER = "http://static.zooniverse.org/www.galaxyzoo.org/images/";
 
     //This is one big image file containing many icons,
