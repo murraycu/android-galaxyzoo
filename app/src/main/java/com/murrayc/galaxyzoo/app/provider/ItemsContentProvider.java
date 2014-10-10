fde8de0551a416e91b26dbfee879f62a3c353ba4
Murray Cumming <murrayc@murrayc.com>
2014-10-10 11:47:42 +0200

    ItemsContentProvider: Move most network-using code into ZooniverseClient.
    This makes ItemsContentProvider smaller and should make it easier to test our use of HTTP.

--- app/src/main/java/com/murrayc/galaxyzoo/app/provider/ItemsContentProvider.java
+++ app/src/main/java/com/murrayc/galaxyzoo/app/provider/ItemsContentProvider.java
@@ -39,7 +39,6 @@ import android.os.ParcelFileDescriptor;
 import android.preference.PreferenceManager;
 import android.provider.BaseColumns;
 import android.text.TextUtils;
-import android.util.Base64;
 
 import com.murrayc.galaxyzoo.app.Log;
 import com.murrayc.galaxyzoo.app.LoginUtils;
@@ -49,17 +48,10 @@ import com.murrayc.galaxyzoo.app.Utils;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.lang.ref.WeakReference;
-import java.net.HttpURLConnection;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -226,11 +218,6 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
 
     }
 
-    //This is an attempt to reduce the amount of Network and Disk IO
-    //that the system does, because even when using a Thread (with Thread.MIN_PRIORITY) instead of
-    //AsyncTask, the UI is non responsive during this work.
-    //For instance, buttons appear to be pressed, but their clicked listeners are not called.
-    private static final int MAXIMUM_DOWNLOAD_ITEMS = 2;
 
     private int mUploadsInProgress = 0;
     private DatabaseHelper mOpenDbHelper;
@@ -240,6 +227,8 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
     /* A map of remote URIs to the last dates that we tried to download them.
      */
     private Map<String, Date> mImageDownloadsInProgress = new HashMap<>();
+
+    private ZooniverseClient mClient = null;
 
     public ItemsContentProvider() {
     }
@@ -845,6 +834,8 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
 
     @Override
     public boolean onCreate() {
+        mClient = new ZooniverseClient(getContext());
+
         mOpenDbHelper = new DatabaseHelper(getContext());
         //This is useful to wipe the database when testing.
         //mOpenDbHelper.onUpgrade(mOpenDbHelper.getWritableDatabase(), 0, 1);
@@ -886,7 +877,7 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
                  * We do this synchronously, waiting for the result,
                  * so we can return the result to the caller.
                  */
-                final LoginUtils.LoginResult result = loginSync(username, password);
+                final LoginUtils.LoginResult result = ZooniverseClient.loginSync(username, password);
                 if (result == null) {
                     return null;
                 }
@@ -1127,7 +1118,7 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
                     //Immediately get some more from the REST server and then try again.
                     //Get one synchronously, for now.
                     try {
-                        final List<MoreItemsJsonParser.Subject> subjects = requestMoreItemsSync(1);
+                        final List<MoreItemsJsonParser.Subject> subjects = mClient.requestMoreItemsSync(1);
                         addSubjects(subjects, false /* not async - we need it immediately. */);
                     } catch (final HttpUtils.NoNetworkException e) {
                         //Return the empty cursor,
@@ -1547,48 +1538,8 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
         return result;
     }
 
-    /** This will not always provide as many items as requested.
-     *
-     * @param count
-     * @return
-     */
-    private List<MoreItemsJsonParser.Subject> requestMoreItemsSync(int count) {
-        throwIfNoNetwork();
-
-        //Avoid suddenly doing too much network and disk IO
-        //as we download too many images.
-        if (count > MAXIMUM_DOWNLOAD_ITEMS) {
-            count = MAXIMUM_DOWNLOAD_ITEMS;
-        }
-
-        //TODO: Can we use Java 7's try-with-resources to automatically close()
-        //the InputStream even though none of the code here should throw an
-        //exception?
-        //Not that the server often won't give us as many as we want,
-        //so a subsequent request might be needed to get all of them.
-        //We don't need to check how many we get, and ask again,
-        //because we already just call queueRegularTasks() again if necessary.
-        final InputStream in = HttpUtils.httpGetRequest(getQueryUri(count));
-        if (in == null) {
-            return null;
-        }
-
-        final List<MoreItemsJsonParser.Subject> result = MoreItemsJsonParser.parseMoreItemsResponseContent(in);
-        try {
-            in.close();
-        } catch (IOException e) {
-            Log.error("requestMoreItemsSync(): Can't close input stream", e);
-        }
-
-        return result;
-    }
-
     private void throwIfNoNetwork() {
         HttpUtils.throwIfNoNetwork(getContext());
-    }
-
-    private String getQueryUri(final int count) {
-        return Config.QUERY_URI + Integer.toString(count); //TODO: Is Integer.toString() locale-dependent?
     }
 
     private void onQueryTaskFinished(final List<MoreItemsJsonParser.Subject> result) {
@@ -1633,123 +1584,6 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
                 + ".glom");
     }
     */
-
-    private static LoginUtils.LoginResult loginSync(final String username, final String password) {
-        final HttpURLConnection conn = HttpUtils.openConnection(Config.LOGIN_URI);
-        if (conn == null) {
-            return null;
-        }
-
-        final List<NameValuePair> nameValuePairs = new ArrayList<>();
-        nameValuePairs.add(new BasicNameValuePair("username", username));
-        nameValuePairs.add(new BasicNameValuePair("password", password));
-
-        try {
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-
-            if (!writeParamsToHttpPost(conn, nameValuePairs)) {
-                return null;
-            }
-
-            conn.connect();
-        } catch (final IOException e) {
-            Log.error("loginSync(): exception during HTTP connection", e);
-
-            return null;
-        }
-
-        //Get the response:
-        InputStream in = null;
-        try {
-            in = conn.getInputStream();
-            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                Log.error("loginSync(): response code: " + conn.getResponseCode());
-                return null;
-            }
-
-            return LoginUtils.parseLoginResponseContent(in);
-        } catch (final IOException e) {
-            Log.error("loginSync(): exception during HTTP connection", e);
-
-            return null;
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (final IOException e) {
-                    Log.error("loginSync(): exception while closing in", e);
-                }
-            }
-        }
-    }
-
-    private static String getPostDataBytes(final List<NameValuePair> nameValuePairs) {
-        final StringBuilder result = new StringBuilder();
-        boolean first = true;
-
-        for (final NameValuePair pair : nameValuePairs) {
-            if (first) {
-                first = false;
-            } else {
-                result.append("&");
-            }
-
-            try {
-                result.append(URLEncoder.encode(pair.getName(), "UTF-8"));
-                result.append("=");
-                result.append(URLEncoder.encode(pair.getValue(), "UTF-8"));
-            } catch (UnsupportedEncodingException e) {
-                Log.error("getPostDataBytes(): Exception", e);
-                return null;
-            }
-        }
-
-        return result.toString();
-    }
-
-    private static boolean writeParamsToHttpPost(final HttpURLConnection conn, final List<NameValuePair> nameValuePairs) {
-        OutputStream out = null;
-        try {
-            out = conn.getOutputStream();
-
-            BufferedWriter writer = null;
-            try {
-                writer = new BufferedWriter(
-                        new OutputStreamWriter(out, "UTF-8"));
-                writer.write(getPostDataBytes(nameValuePairs));
-                writer.flush();
-            } catch (final IOException e) {
-                Log.error("writeParamsToHttpPost(): Exception: ", e);
-                return false;
-            } finally {
-                if (writer != null) {
-                    writer.close();
-                }
-            }
-        } catch (final IOException e) {
-            Log.error("writeParamsToHttpPost(): Exception: ", e);
-            return false;
-        } finally {
-            if (out != null) {
-                try {
-                    out.close();
-                } catch (final IOException e) {
-                    Log.error("writeParamsToHttpPost(): Exception while closing out", e);
-                }
-            }
-        }
-
-        return true;
-    }
-
-    private String generateAuthorizationHeader(final String authName, final String authApiKey) {
-        //See the similar code in Zooniverse's user.coffee source code:
-        //https://github.com/zooniverse/Zooniverse/blob/master/src/models/user.coffee#L49
-        final String str = authName + ":" + authApiKey;
-        return "Basic " + Base64.encodeToString(str.getBytes(), Base64.DEFAULT);
-    }
 
     private void onUploadTaskFinished(boolean result, final String itemId) {
         if (result) {
@@ -2003,7 +1837,7 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
             //http://developer.android.com/reference/android/os/AsyncTask.html
             final int count = params[0];
 
-            return requestMoreItemsSync(count);
+            return mClient.requestMoreItemsSync(count);
         }
 
         @Override
@@ -2015,29 +1849,6 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
     }
 
     private Boolean doUploadSync(final String itemId, final String subjectId, final String authName, final String authApiKey) {
-        final HttpURLConnection conn = HttpUtils.openConnection(Config.POST_URI);
-        if (conn == null) {
-            return false;
-        }
-
-        try {
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-        } catch (final IOException e) {
-            Log.error("UploadAsyncTask.doInBackground(): exception during HTTP connection", e);
-
-            return false;
-        }
-
-
-        //Add the authentication details to the headers;
-        //Be careful: The server still returns OK_CREATED even if we provide the wrong Authorization here.
-        //There doesn't seem to be any way to know if it's correct other than checking your recent
-        //classifications in your profile.
-        if ((authName != null) && (authApiKey != null)) {
-            conn.setRequestProperty("Authorization", generateAuthorizationHeader(authName, authApiKey));
-        }
 
         final String PARAM_PART_CLASSIFICATION = "classification";
 
@@ -2085,8 +1896,6 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
                     null, null, orderBy);
         }
 
-
-        //List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
         while (c.moveToNext()) {
             final int sequence = c.getInt(0);
             final String questionId = c.getString(1);
@@ -2125,36 +1934,8 @@ public class ItemsContentProvider extends ContentProvider implements SharedPrefe
 
         c.close();
 
-        if (!writeParamsToHttpPost(conn, nameValuePairs)) {
-            return false;
-        }
 
-        //TODO: Is this necessary? conn.connect();
-
-        //Get the response:
-        InputStream in = null;
-        try {
-            in = conn.getInputStream();
-            final int responseCode = conn.getResponseCode();
-            if (responseCode != HttpURLConnection.HTTP_CREATED) {
-                Log.error("UploadAsyncTask.doInBackground(): Did not receive the 201 Created status code: " + conn.getResponseCode());
-                return false;
-            }
-
-            return true;
-        } catch (final IOException e) {
-            Log.error("UploadAsyncTask.doInBackground(): exception during HTTP connection", e);
-
-            return false;
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (final IOException e) {
-                    Log.error("UploadAsyncTask.doInBackground(): exception while closing in", e);
-                }
-            }
-        }
+        return mClient.doUpload(authName, authApiKey, nameValuePairs);
     }
 
     private class UploadAsyncTask extends AsyncTask<String, Integer, Boolean> {
---
+++ app/src/main/java/com/murrayc/galaxyzoo/app/provider/ZooniverseClient.java
@@ -0,0 +1,259 @@
+package com.murrayc.galaxyzoo.app.provider;
+
+import android.content.Context;
+import android.util.Base64;
+
+import com.murrayc.galaxyzoo.app.Log;
+import com.murrayc.galaxyzoo.app.LoginUtils;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by murrayc on 10/10/14.
+ */
+public class ZooniverseClient {
+    private final Context mContext;
+
+    //This is an attempt to reduce the amount of Network and Disk IO
+    //that the system does, because even when using a Thread (with Thread.MIN_PRIORITY) instead of
+    //AsyncTask, the UI is non responsive during this work.
+    //For instance, buttons appear to be pressed, but their clicked listeners are not called.
+    private static final int MAXIMUM_DOWNLOAD_ITEMS = 2;
+
+    ZooniverseClient(final Context context) {
+        mContext = context;
+    }
+
+    static LoginUtils.LoginResult loginSync(final String username, final String password) {
+        final HttpURLConnection conn = HttpUtils.openConnection(Config.LOGIN_URI);
+        if (conn == null) {
+            return null;
+        }
+
+        final List<NameValuePair> nameValuePairs = new ArrayList<>();
+        nameValuePairs.add(new BasicNameValuePair("username", username));
+        nameValuePairs.add(new BasicNameValuePair("password", password));
+
+        try {
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+
+            if (writeParamsToHttpPost(conn, nameValuePairs)) {
+                return null;
+            }
+
+            conn.connect();
+        } catch (final IOException e) {
+            Log.error("loginSync(): exception during HTTP connection", e);
+
+            return null;
+        }
+
+        //Get the response:
+        InputStream in = null;
+        try {
+            in = conn.getInputStream();
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                Log.error("loginSync(): response code: " + conn.getResponseCode());
+                return null;
+            }
+
+            return LoginUtils.parseLoginResponseContent(in);
+        } catch (final IOException e) {
+            Log.error("loginSync(): exception during HTTP connection", e);
+
+            return null;
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (final IOException e) {
+                    Log.error("loginSync(): exception while closing in", e);
+                }
+            }
+        }
+    }
+
+    static boolean writeParamsToHttpPost(final HttpURLConnection conn, final List<NameValuePair> nameValuePairs) {
+        OutputStream out = null;
+        try {
+            out = conn.getOutputStream();
+
+            BufferedWriter writer = null;
+            try {
+                writer = new BufferedWriter(
+                        new OutputStreamWriter(out, "UTF-8"));
+                writer.write(getPostDataBytes(nameValuePairs));
+                writer.flush();
+            } catch (final IOException e) {
+                Log.error("writeParamsToHttpPost(): Exception: ", e);
+                return false;
+            } finally {
+                if (writer != null) {
+                    writer.close();
+                }
+            }
+        } catch (final IOException e) {
+            Log.error("writeParamsToHttpPost(): Exception: ", e);
+            return false;
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (final IOException e) {
+                    Log.error("writeParamsToHttpPost(): Exception while closing out", e);
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static String getPostDataBytes(final List<NameValuePair> nameValuePairs) {
+        final StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        for (final NameValuePair pair : nameValuePairs) {
+            if (first) {
+                first = false;
+            } else {
+                result.append("&");
+            }
+
+            try {
+                result.append(URLEncoder.encode(pair.getName(), "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(pair.getValue(), "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                Log.error("getPostDataBytes(): Exception", e);
+                return null;
+            }
+        }
+
+        return result.toString();
+    }
+
+    private String generateAuthorizationHeader(final String authName, final String authApiKey) {
+        //See the similar code in Zooniverse's user.coffee source code:
+        //https://github.com/zooniverse/Zooniverse/blob/master/src/models/user.coffee#L49
+        final String str = authName + ":" + authApiKey;
+        return "Basic " + Base64.encodeToString(str.getBytes(), Base64.DEFAULT);
+    }
+
+    /** This will not always provide as many items as requested.
+     *
+     * @param count
+     * @return
+     */
+    List<MoreItemsJsonParser.Subject> requestMoreItemsSync(int count) {
+        throwIfNoNetwork();
+
+        //Avoid suddenly doing too much network and disk IO
+        //as we download too many images.
+        if (count > MAXIMUM_DOWNLOAD_ITEMS) {
+            count = MAXIMUM_DOWNLOAD_ITEMS;
+        }
+
+        //TODO: Can we use Java 7's try-with-resources to automatically close()
+        //the InputStream even though none of the code here should throw an
+        //exception?
+        //Not that the server often won't give us as many as we want,
+        //so a subsequent request might be needed to get all of them.
+        //We don't need to check how many we get, and ask again,
+        //because we already just call queueRegularTasks() again if necessary.
+        final InputStream in = HttpUtils.httpGetRequest(getQueryUri(count));
+        if (in == null) {
+            return null;
+        }
+
+        final List<MoreItemsJsonParser.Subject> result = MoreItemsJsonParser.parseMoreItemsResponseContent(in);
+        try {
+            in.close();
+        } catch (IOException e) {
+            Log.error("requestMoreItemsSync(): Can't close input stream", e);
+        }
+
+        return result;
+    }
+
+    private String getQueryUri(final int count) {
+        return Config.QUERY_URI + Integer.toString(count); //TODO: Is Integer.toString() locale-dependent?
+    }
+
+    private void throwIfNoNetwork() {
+        HttpUtils.throwIfNoNetwork(getContext());
+    }
+
+    private Context getContext() {
+        return mContext;
+    }
+
+    boolean doUpload(final String authName, final String authApiKey, final List<NameValuePair> nameValuePairs) {
+        final HttpURLConnection conn = HttpUtils.openConnection(Config.POST_URI);
+        if (conn == null) {
+            return false;
+        }
+
+        try {
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+        } catch (final IOException e) {
+            Log.error("UploadAsyncTask.doInBackground(): exception during HTTP connection", e);
+
+            return false;
+        }
+
+        //Add the authentication details to the headers;
+        //Be careful: The server still returns OK_CREATED even if we provide the wrong Authorization here.
+        //There doesn't seem to be any way to know if it's correct other than checking your recent
+        //classifications in your profile.
+        if ((authName != null) && (authApiKey != null)) {
+            conn.setRequestProperty("Authorization", generateAuthorizationHeader(authName, authApiKey));
+        }
+
+        if (!writeParamsToHttpPost(conn, nameValuePairs)) {
+            return false;
+        }
+
+        //TODO: Is this necessary? conn.connect();
+
+        //Get the response:
+        InputStream in = null;
+        try {
+            in = conn.getInputStream();
+            final int responseCode = conn.getResponseCode();
+            if (responseCode != HttpURLConnection.HTTP_CREATED) {
+                Log.error("UploadAsyncTask.doInBackground(): Did not receive the 201 Created status code: " + conn.getResponseCode());
+                return false;
+            }
+
+            return true;
+        } catch (final IOException e) {
+            Log.error("UploadAsyncTask.doInBackground(): exception during HTTP connection", e);
+
+            return false;
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (final IOException e) {
+                    Log.error("UploadAsyncTask.doInBackground(): exception while closing in", e);
+                }
+            }
+        }
+    }
+}
