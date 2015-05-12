722740af2966bd4c00149a055979783558052559
Murray Cumming <murrayc@murrayc.com>
2015-05-12 09:46:18 +0200

    Add an Application subclass, as a useful place to initialize some debug stuff.
    The Application documentation suggests using a separate singelton for most stuff,
    which we already do:
    http://developer.android.com/reference/android/app/Application.html
    However, this seems like the right place to do library initialization.

---
+++ app/src/main/java/com/murrayc/galaxyzoo/app/GalaxyZooApplication.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2015 Murray Cumming
+ *
+ * This file is part of android-galaxyzoo.
+ *
+ * android-galaxyzoo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * android-galaxyzoo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with android-galaxyzoo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.murrayc.galaxyzoo.app;
+
+import android.app.Application;
+
+/**
+ * Created by murrayc on 5/12/15.
+ */
+public class GalaxyZooApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
