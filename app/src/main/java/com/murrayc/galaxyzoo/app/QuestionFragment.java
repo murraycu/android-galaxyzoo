7fafda1ec33770197a4213e6f8c7314b18600d6c
Murray Cumming <murrayc@murrayc.com>
2015-07-03 13:16:01 +0200

    QuestionFragment.updateFromCursor: Log when we have to use the default group ID.
    This should only happen for subjects that were downloaded ages ago by a very
    old version of the app that didn't store the group ID.
    
    This might, but probably won't, be relevant to
    https://github.com/murraycu/android-galaxyzoo/issues/22

--- app/src/main/java/com/murrayc/galaxyzoo/app/QuestionFragment.java
+++ app/src/main/java/com/murrayc/galaxyzoo/app/QuestionFragment.java
@@ -827,6 +827,7 @@ public class QuestionFragment extends BaseQuestionFragment
 
         String groupId = mCursor.getString(COLUMN_INDEX_GROUP_ID);
         if (TextUtils.isEmpty(groupId)) {
+            Log.error("updateFromCursor(): We don't have a group ID. Using a default.");
             //Assume that this is an old cached item from before we added the group ID
             //database field, when we only use the sloan group:
             groupId = com.murrayc.galaxyzoo.app.provider.Config.SUBJECT_GROUP_ID_SLOAN;
