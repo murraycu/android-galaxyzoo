41ac5cae12f4f144249bb9a6d14f87e861f636f6
Murray Cumming <murrayc@murrayc.com>
2015-07-08 09:49:07 +0200

    Config: Add a commented-out SERVER URI for testing.

--- app/src/main/java/com/murrayc/galaxyzoo/app/provider/Config.java
+++ app/src/main/java/com/murrayc/galaxyzoo/app/provider/Config.java
@@ -32,7 +32,10 @@ import java.util.Map;
  */
 public final class Config {
 
+    //The real server:
     public static final String SERVER = "https://api.zooniverse.org/projects/galaxy_zoo/";
+    //For testing:
+    //public static final String SERVER = "https://dev.zooniverse.org/projects/galaxy_zoo/";
 
     public static class SubjectGroup {
         final String filename;
