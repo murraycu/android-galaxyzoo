56b88476a34d743d5f7d02100904d720d5801de5
Murray Cumming <murrayc@murrayc.com>
2015-09-06 12:06:59 +0200

    Config: Use the decals tree.

--- app/src/main/java/com/murrayc/galaxyzoo/app/provider/Config.java
+++ app/src/main/java/com/murrayc/galaxyzoo/app/provider/Config.java
@@ -85,6 +85,8 @@ public final class Config {
 
     //See Config.coffee:production:
     //https://github.com/zooniverse/Galaxy-Zoo/blob/master/app/lib/config.coffee
+    public static final String SUBJECT_GROUP_ID_DECALS = "55db7cf01766276e7b000001";
+    //TODO: public static final String SUBJECT_GROUP_ID_ILLUSTRIS = "55db71251766276613000001";
     public static final String SUBJECT_GROUP_ID_SLOAN = "50251c3b516bcb6ecb000002";
     public static final String SUBJECT_GROUP_ID_SLOAN_SINGLEBAND = "5514521e2f0eef2012000001";
     public static final String SUBJECT_GROUP_ID_GOODS_FULL = "551453e12f0eef21f2000001";
@@ -96,14 +98,17 @@ public final class Config {
 
         //Production:
         {
-            SUBJECT_GROUPS.put(SUBJECT_GROUP_ID_SLOAN, //SSID / sloan
-                    new SubjectGroup("sloan_tree.xml", true,
-                            new DecisionTree.DiscussQuestion("sloan-11", "a-0", "a-1")));
+            SUBJECT_GROUPS.put(SUBJECT_GROUP_ID_DECALS, //SSID / Decals
+                    new SubjectGroup("decals_tree.xml", true,
+                            new DecisionTree.DiscussQuestion("decals-11", "a-0", "a-1")));
 
             //We don't request items for all these groups any more, but we still want to load the
             //trees so can ask questions about items that have already been downloaded and stored in
             //the cache.
             //At some point we can remove some when we are sure they are unnecessary.
+            SUBJECT_GROUPS.put(SUBJECT_GROUP_ID_SLOAN, //SSID / sloan
+                    new SubjectGroup("sloan_tree.xml", false,
+                            new DecisionTree.DiscussQuestion("sloan-11", "a-0", "a-1")));
             SUBJECT_GROUPS.put("551456e02f0eef2535000001", // candels_2epoch
                     new SubjectGroup("candels_tree.xml", false,
                             new DecisionTree.DiscussQuestion("candels-17", "a-0", "a-1")));
