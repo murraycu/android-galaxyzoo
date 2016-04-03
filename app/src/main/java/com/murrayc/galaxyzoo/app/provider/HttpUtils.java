36abdb8848681b500bd8bc8ead1938303ed77f73
Murray Cumming <murrayc@murrayc.com>
2016-04-03 13:08:58 +0200

    Revert "Increase timeout and retry for better user experience under slow network"
    This reverts commit 1f2c2e433cc8401ef85d37d39ae128860a040fa9.
    
    Because this seems to be leading to volley timeouts.

--- app/src/main/java/com/murrayc/galaxyzoo/app/provider/HttpUtils.java
+++ app/src/main/java/com/murrayc/galaxyzoo/app/provider/HttpUtils.java
@@ -26,7 +26,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Base64;
 
-import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -112,7 +111,6 @@ public final class HttpUtils {
         //so don't waste memory or storage caching it.
         //(We are downloading it to our own cache, of course.)
         request.setShouldCache(false);
-        request.setRetryPolicy(new DefaultRetryPolicy(30, 2, 1f));
 
         requestQueue.add(request);
 
