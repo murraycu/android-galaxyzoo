5f035815b1ef7ffa5545d3fb225eee5475bb6d6a
Murray Cumming <murrayc@murrayc.com>
2016-08-21 16:55:01 +0200

    SubjectViewerActivityTest: Update deprecated ActivityUnitTestCase to JUnit 4.

--- app/src/androidTest/java/com/murrayc/galaxyzoo/app/test/SubjectViewerActivityTest.java
+++ app/src/androidTest/java/com/murrayc/galaxyzoo/app/test/SubjectViewerActivityTest.java
@@ -19,44 +19,67 @@
 
 package com.murrayc.galaxyzoo.app.test;
 
+import android.app.Instrumentation;
 import android.content.Intent;
-import android.test.ActivityUnitTestCase;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import com.murrayc.galaxyzoo.app.SubjectViewerActivity;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * Created by murrayc on 5/26/14.
  */
-public class SubjectViewerActivityTest
-        extends ActivityUnitTestCase<SubjectViewerActivity> {
+@RunWith(AndroidJUnit4.class)
+public class SubjectViewerActivityTest {
+    // This must be public, or we'll get this exception:
+    // org.junit.internal.runners.rules.ValidationError: The @Rule 'testRule' must be public
+    // Note: If the third argument (launchActivity) is not false (really), the tests will fail because the
+    // activity cannot be launched when you call launchActivity().
+    @Rule
+    public ActivityTestRule<SubjectViewerActivity> testRule = new ActivityTestRule<>(SubjectViewerActivity.class, false, false);
 
     private SubjectViewerActivity mActivity;
 
-    public SubjectViewerActivityTest() {
-        super(SubjectViewerActivity.class);
-    }
-
-    protected void setUp() throws Exception {
-        super.setUp();
-
-        TestUtils.setTheme(this);
-
-        startActivity(new Intent(getInstrumentation().getTargetContext(), SubjectViewerActivity.class), null, null);
-
-        mActivity = getActivity();
+    @Before
+    public void setUp() throws Exception {
+        final Intent intent = new Intent();
+        mActivity = testRule.launchActivity(intent);
         assertNotNull(mActivity);
     }
 
+    @Test
     public void testExists() {
         assertNotNull(mActivity);
     }
 
+    @Test
     public void testStateDestroy() {
         final String TEST_ITEM_ID = "test123456789";
         mActivity.setItemId(TEST_ITEM_ID);
         mActivity.finish();
-        mActivity = this.getActivity();
-        assertNotNull(mActivity);
+
+        // Based on this:
+        // http://stackoverflow.com/a/33213279/1123654
+        final Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+        instrumentation.runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                mActivity.recreate();
+            }
+        });
+
         assertEquals(TEST_ITEM_ID, mActivity.getItemId());
+
+        //TODO: Do something like this too:
+        //onView(withText("a string depending on XXX value")).check(doesNotExist())
     }
 }
