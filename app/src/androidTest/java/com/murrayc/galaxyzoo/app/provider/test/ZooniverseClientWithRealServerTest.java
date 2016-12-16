c09c9c4d9fa3bd81cb455597e9ae1b62e8210c94
Murray Cumming <murrayc@murrayc.com>
2016-12-16 08:41:17 +0100

    Add test to make sure that all survey actually work with server.
    This should find errors in the survey group IDs.

---
+++ app/src/androidTest/java/com/murrayc/galaxyzoo/app/provider/test/ZooniverseClientWithRealServerTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2014 Murray Cumming
+ *
+ * This file is part of android-glom
+ *
+ * android-glom is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * android-glom is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with android-glom.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.murrayc.galaxyzoo.app.provider.test;
+
+import android.app.Instrumentation;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.text.TextUtils;
+
+import com.murrayc.galaxyzoo.app.provider.Config;
+import com.murrayc.galaxyzoo.app.provider.client.ZooniverseClient;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Simple test to ensure that the generated bindings are working.
+ */
+public class ZooniverseClientWithRealServerTest {
+    @Before
+    public void setUp() throws IOException {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testMoreItems() throws IOException, ZooniverseClient.RequestMoreItemsException {
+
+        final ZooniverseClient client = createZooniverseClient();
+
+        // Do this enough times that we are very likely to excercise all groups:
+        final int count = Config.SUBJECT_GROUPS.size() * 5;
+        for (int i = 0; i < count; ++i) {
+            final List<ZooniverseClient.Subject> subjects = client.requestMoreItemsSync(1);
+            assertNotNull(subjects);
+            assertTrue(subjects.size() == 1);
+
+            final ZooniverseClient.Subject subject = subjects.get(0);
+            assertNotNull(subject);
+            assertFalse(TextUtils.isEmpty(subject.getSubjectId()));
+        }
+    }
+
+    private static ZooniverseClient createZooniverseClient() {
+        final Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+        final Context context = instrumentation.getTargetContext();
+        return new ZooniverseClient(context, Config.SERVER);
+    }
+}
